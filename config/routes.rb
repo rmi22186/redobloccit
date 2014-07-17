5a63b4d8e625f8b264bcfe8386ff14e568a3fe0e
Robert Ing <rmi22186@gmail.com>
2014-07-17 11:47:39 -0700

    Created welcome controller with index and about views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Bloccit::Application.routes.draw do
+  get "welcome/index"
+
+  get "welcome/about"
+
+  root to: 'welcome#index'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +78,3 @@ Bloccit::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
