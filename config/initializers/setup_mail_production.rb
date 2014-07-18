f3a2fd304535635e42a354256721db048f560dc8
Robert Ing <rmi22186@gmail.com>
2014-07-18 16:15:26 -0700

    fix mail setup

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
     :port       => '587',
     :authentication =>  :plain,
     :user_name  =>  ENV['SENDGRID_USERNAME'],
-    :password   =>  ENV['SENDGRID_PASSWORD',
+    :password   =>  ENV['SENDGRID_PASSWORD'],
     :domain     => 'heroku.com',
     :enable_starttls_auto => true
   }
