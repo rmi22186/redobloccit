1f0df214842e9213fba67c0b5b323fdf5ad0ad3a
Robert Ing <rmi22186@gmail.com>
2014-07-22 17:30:14 -0700

    add destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,4 +18,21 @@ class CommentsController < ApplicationController
       flash[:error] = "There was an error saving the comment. Please try again."
     end
   end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    
+    @comment = @post.comments.find(params[:id])
+
+    authorize! :destroy, @comment, message: "You need to own the comment to delete it."
+
+    if @comment.destroy
+      flash[:notice] = "Comment destroyed successfully."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error deleting your comment."
+      redirect_to [@topic, @post]
+    end
+  end
 end
