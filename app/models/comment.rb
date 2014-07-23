7d7f7a75c8de861ba90b63c92f8af88dc5585dab
Robert Ing <rmi22186@gmail.com>
2014-07-23 12:55:42 -0700

    Implemented voting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
-  attr_accessible :body
+  attr_accessible :body, :post
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
 require 'faker'
 
-#create 15 topics
-
+# Create 15 topics
 topics = []
 15.times do
   topics << Topic.create(
-    name: Faker::Lorem.words(rand(1..10)).join(" "),
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
     description: Faker::Lorem.paragraph(rand(1..4))
-    )
+  )
 end
 
 (10).times do
@@ -23,19 +22,28 @@ end
   rand(5..12).times do
     topic = topics.first
     p = u.posts.create(
+      topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
 
+    p.update_rank
     topics.rotate!
-
-    rand(3..7).times do
-      p.comments.create(
-        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
-    end
   end
 end
+
+post_count = Post.count
+User.all.each do |user|
+  rand(30..50).times do
+    p = Post.find(rand(1..post_count))
+    c = user.comments.create(
+      body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"),
+      post: p)
+    c.update_attribute(:created_at, Time.now - rand(600..31536000))
+  end
+end
+
 
 
 u = User.new(
