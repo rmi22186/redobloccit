bd8ccbab30809362e5f9f1fa5eb1d12d737554ab
Robert Ing <rmi22186@gmail.com>
2014-07-23 17:04:07 -0700

    scoped topics for privacy

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
       can :manage, Comment, :user_id => user.id
       can :create, Vote
       can :manage, Favorite, :user_id => user.id
+      can :read, Topic
     end
 
     if user.role? :moderator
@@ -20,6 +21,7 @@ class Ability
       can :manage, :all
     end
 
-    can :read, :all
+    can :read, Topic, public: true
+    can :read, Post 
   end
 end
