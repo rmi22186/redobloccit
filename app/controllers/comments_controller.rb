65740d36a30083fa68aed7346d5d476dc7132feb
Robert Ing <rmi22186@gmail.com>
2014-08-11 15:50:29 -0700

    ajaxified

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
+
   def create
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
@@ -24,15 +26,16 @@ class CommentsController < ApplicationController
     @post = @topic.posts.find(params[:post_id])
     
     @comment = @post.comments.find(params[:id])
-
     authorize! :destroy, @comment, message: "You need to own the comment to delete it."
 
     if @comment.destroy
       flash[:notice] = "Comment destroyed successfully."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error deleting your comment."
-      redirect_to [@topic, @post]
+    end
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
     end
   end
 end
