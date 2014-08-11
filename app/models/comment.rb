ea0edf0a757e14dd75db5ba8de5285d26e158078
Robert Ing <rmi22186@gmail.com>
2014-08-11 16:50:39 -0700

    Ajaxified comment creation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   attr_accessible :body, :post
 
+  default_scope order('updated_at DESC')
+
   after_create :send_favorite_emails
 
   private
